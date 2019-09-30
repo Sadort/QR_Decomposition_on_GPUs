f82a84385f70e28fb2b2710a3fe2b46ba8ef4849
Sadort <hhsadort@gmail.com>
2019-09-30 12:06:17 +0000

    incorrect result

--- qr_code.cu
+++ qr_code.cu
@@ -26,26 +26,43 @@ __global__ void update_beta(float *d_beta, float *d_dotpro)
 
 void house(float *d_v, float *d_x, float *d_beta, int len, int m, int n)
 {
+    printf("1\n");
     cudaError_t cudaStat;
+    printf("2\n");
     cublasStatus_t stat;
+    printf("3\n");
     cublasHandle_t handle;
-    
+    printf("4\n");
+    cublasCreate(&handle);
+    printf("5\n");
+    cublasSetPointerMode(handle, CUBLAS_POINTER_MODE_DEVICE);
+    printf("6\n");
     float *d_norm, *d_dotpro;
     cudaStat = cudaMalloc((void**)&d_norm, sizeof(float) * 1);
     cudaStat = cudaMalloc((void**)&d_dotpro, sizeof(float) * 1);
+    printf("point0\n");
     cudaStat = cudaMemset(d_norm, 0, sizeof(float) * 1);
     cudaStat = cudaMemset(d_dotpro, 0, sizeof(float) * 1);
-    
-    stat = cublasCreate(&handle);
+    cudaDeviceSynchronize();
+    printf("point1\n");
+//    cublasSetPointerMode(handle, CUBLAS_POINTER_MODE_DEVICE);  
     stat = cublasSnrm2(handle, len, d_x, 1, d_norm);
-    
+    cudaDeviceSynchronize();
+
+    printf("point2\n");
+//    cublasSetPointerMode(handle, CUBLAS_POINTER_MODE_DEVICE);
     stat = cublasScopy(handle, len, d_x, 1, d_v, 1);
+    cudaDeviceSynchronize();
     update_diagonal<<<1, 1>>>(d_v, d_x, d_norm);
-    
+    cudaDeviceSynchronize();
+    printf("point3\n");
+//    cublasSetPointerMode(handle, CUBLAS_POINTER_MODE_DEVICE);
     stat = cublasSdot(handle, len, d_v, 1, d_v, 1, d_dotpro);
-    
+    cudaDeviceSynchronize();
+    printf("point4\n");
     update_beta<<<1, 1>>>(d_beta, d_dotpro);
-    
+    cudaDeviceSynchronize();
+    printf("point5\n");
     cublasDestroy(handle);
     cudaFree(d_norm);
     cudaFree(d_dotpro);
@@ -58,21 +75,35 @@ void apply_house(float *d_v, float *d_A, float *d_beta, int len, int m, int n)
     cudaError_t cudaStat;
     cublasStatus_t stat;
     cublasHandle_t handle;
-    
+    cublasCreate(&handle);
+    cublasSetPointerMode(handle, CUBLAS_POINTER_MODE_DEVICE);
+
     int sub_m = len;
     int sub_n = n - m + len;
     
     float *d_v_A;
+    printf("point1\n");
     cudaStat = cudaMalloc((void**)&d_v_A, sizeof(float) * sub_n);
+    printf("point2\n");
     cudaStat = cudaMemset(d_v_A, 0, sizeof(float) * sub_n);
-    stat = cublasCreate(&handle);
-    
-    float alpha = 1, beta = 0;
-    stat = cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, 1, sub_n, sub_m, &alpha, d_v, sub_m, d_A, m, &beta, d_v_A, 1);
-    
-    beta = 1;
-    stat = cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, sub_m, sub_n, 1, d_beta, d_v, sub_m, d_v_A, 1, &beta, d_A, sub_m);
-    
+
+    float *alpha;
+    float *beta;
+    cudaStat = cudaMalloc((void**)&alpha, sizeof(float) * 1);
+    cudaStat = cudaMalloc((void**)&beta, sizeof(float) * 1);
+    cudaStat = cudaMemset(alpha, 1, sizeof(float) * 1);
+    cudaStat = cudaMemset(beta, 0, sizeof(float) * 1);
+    printf("point3\n");
+    cudaDeviceSynchronize();
+    stat = cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, 1, sub_n, sub_m, alpha, d_v, sub_m, d_A, m, beta, d_v_A, 1);
+    cudaDeviceSynchronize();
+
+    cudaStat = cudaMemset(beta, 1, sizeof(float) * 1);
+    printf("point4\n");
+    stat = cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, sub_m, sub_n, 1, d_beta, d_v, sub_m, d_v_A, 1, beta, d_A, sub_m);
+    cudaDeviceSynchronize();
+    printf("point5\n");
+
     cublasDestroy(handle);
     cudaFree(d_v_A);
     
@@ -85,19 +116,25 @@ void qr_calculate(float *d_A, int m, int n)
     cudaError_t cudaStat;
     cublasStatus_t stat;
     cublasHandle_t handle;
+    cublasCreate(&handle);
+    cublasSetPointerMode(handle, CUBLAS_POINTER_MODE_DEVICE);
+    //cublasHandle_t handle;
     
     int len = 0;
     float *d_beta, *d_house_v;
     cudaStat = cudaMalloc((void**)&d_beta,sizeof(float)*1);
     cudaStat = cudaMalloc((void**)&d_house_v,sizeof(float)*m);
-    
+    cudaDeviceSynchronize();
+
     for (int k = 0; k < n; k++) {
         
         //householder reflector
         len = m - k;
+        printf("%d access house()\n", k);
         house(d_house_v, &d_A[IDX2C(k,k,m)], d_beta, len, m, n);
         
         //apply householder reflector
+        printf("%d access apply_house()\n", k);
         apply_house(d_house_v, &d_A[IDX2C(k,k,m)], d_beta, len, m, n);
     }
 }
@@ -111,6 +148,7 @@ int main()
     cudaError_t cudaStat;
     cudaStat = cudaMalloc((void**)&d_A,sizeof(float)*m*n);
     cudaStat = cudaMemcpy(d_A, A, sizeof(float)*m*n, cudaMemcpyHostToDevice);
+    cudaDeviceSynchronize();
     
     qr_calculate(d_A, m, n);
     
@@ -119,6 +157,7 @@ int main()
     for (i = 0; i < m*n; i++) {
         printf("%f ", A[i]);
     }
+    printf("\n");
     
     return 0;
 }
