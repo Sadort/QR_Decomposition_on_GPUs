c7f0dcfc63322de9adf76ceeef25e93faae9bf55
Sadort <hhsadort@gmail.com>
2019-10-08 23:36:36 +0000

    first test

--- qr_code.cpp
+++ qr_code.cpp
@@ -1,5 +1,6 @@
 #include "kernel.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 #define IDX2C(i,j,ld) (((j)*(ld))+(i))
@@ -9,11 +10,11 @@ extern void unblocked_qr_calculate(float *d_A, int m, int n);
 
 void verify(int m, int n)
 {
-    double *X, *Y;
+    float *X, *Y;
     int i;
     int num = m / n;
-    X = (double *)malloc(n*n * sizeof(double));
-    Y = (double *)malloc(n*n * sizeof(double));
+    X = (float *)malloc(n*n * sizeof(float));
+    Y = (float *)malloc(n*n * sizeof(float));
     char filename [50];
     sprintf(filename, "o_%d_%d_%d.txt", num, n, n);
     ifstream in_matlab(filename);
@@ -35,20 +36,25 @@ void verify(int m, int n)
     
     //printMatrix(m, n, X, m, "matlab_result");
     //printMatrix(m, n, Y, m, "cuda_result");
-    
+    int flag = 0;
     for (i = 0; i < n*n; i++) {
-        if ((X[i] - Y[i]) * (X[i] - Y[i]) > 0.0001) {
-            printf("Verification Failed.\n\n");
-            return;
+        if ((X[i] + Y[i]) * (X[i] + Y[i]) > 0.0001 && (X[i] - Y[i]) * (X[i] - Y[i]) > 0.0001) {
+            printf("row %d col %d, %f, %f\n", i/n, i%n, X[i], Y[i]);
+            flag = 1;
         }
+    }
+    if(flag == 1)
+    {
+        printf("Verification Failed.\n\n");
+        return;
     }
     printf("Verification complete.\n\n");
 }
 
 int main()
 {
-    int m = 512, n = 256, i;
-    int r = 8;
+    int m = 512, n = 256, i, j;
+    int r = 64;
     int num = m / n;
     char filename [50];
 //    float A[m*n] = { 1, -1, 4, 1,
@@ -82,16 +88,26 @@ int main()
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
-    float milliseconds = 0;
-    
-    //unblocked_qr_calculate(d_A, m, n);
+    float milliseconds = 0, totalmseconds = 0;
+    int iterations = 20;
+    for(i = 0; i < iterations; i++)
+    {
+        cudaEventRecord(start);
 
-    blocked_qr_calculate(d_A, m, n, r);
-    
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&milliseconds, start, stop);
-    printf("Elapsed time: %f ms.", milliseconds);
+//        unblocked_qr_calculate(d_A, m, n);
+
+        blocked_qr_calculate(d_A, m, n, r);
+
+        cudaDeviceSynchronize();
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&milliseconds, start, stop);
+        totalmseconds += milliseconds;
+        if(i == iterations - 1) break;
+        cudaMemcpy( d_A, A, m*n*sizeof(float), cudaMemcpyHostToDevice );
+    }
+
+    printf("Elapsed time: %2.13f ms.", totalmseconds / iterations);
 
     cudaStat = cudaMemcpy(A, d_A, sizeof(float)*m*n, cudaMemcpyDeviceToHost);
     cudaDeviceSynchronize();
